e6ef008ae469b6957b7abb49805bf54dfce2d525
Heisgo <heisgo3@gmail.com>
2026-08-22 12:44:33 -0300

    Update plague_movement

--- Scenes/Game/plague_movement.gd
+++ Scenes/Game/plague_movement.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
 @export var pos_positions: Array[Marker2D] = []
-@onready var plague: Node2D = $"."
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	plague.position = pos_positions[0].position
+	position = pos_positions[0].position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	plague.position = pos_positions.pick_random().position
+	position = pos_positions.pick_random().position
